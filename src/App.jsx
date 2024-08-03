0a6d1d264b526de25c798d96b7ca2e639124af21
youssef-khalifa <ykhalifa19921@gmail.com>
2024-08-03 22:42:16 +0300

    s

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+const H1= styled.h1`
+  font-size:30;
+  font-weight:600;
+`
+function App() {
+  return (
+    <div>
+      <H1>the wild</H1>
+    </div>
+  )
+}
+
+export default App
