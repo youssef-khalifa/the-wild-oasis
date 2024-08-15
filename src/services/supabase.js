e2c76857339679b4f98819118213f39954217076
youssef-khalifa <68951546+youssef-khalifa@users.noreply.github.com>
2024-08-15 05:58:10 +0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://hnkoqgcfyderpaztebuy.supabase.co";
+export const supabaseUrl = "https://Ur own.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhua29xZ2NmeWRlcnBhenRlYnV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI4MDA0MjAsImV4cCI6MjAzODM3NjQyMH0.d9AM4oJR3uzX17kqaLOb-98PKofaeZWuXmjELCpmpfE";
+  "Ur own";
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
