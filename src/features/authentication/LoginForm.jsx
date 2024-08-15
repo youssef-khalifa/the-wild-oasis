7507da6c4cc1d5d266b9d6fb295056b71f4ca529
youssef-khalifa <ykhalifa19921@gmail.com>
2024-08-15 05:50:54 +0300

    s

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("weloyot533@acpeak.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
